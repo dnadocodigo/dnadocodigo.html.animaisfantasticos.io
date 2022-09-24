86e6b7639d74ca4393847082d6daaf5afe47f1c3
developer's Brasil DBA <dnadevnet@gmail.com>
2022-09-24 04:00:54 -0300

    js adicioned function Soft Scroll

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import tabMenuInit from "../js/modules/tabMenu.js";
 import accordionListInit from "../js/modules/accordionList.js";
+import softScrollInit from "../js/modules/softScroll.js";
 
 
 
 tabMenuInit();
 accordionListInit();
+softScrollInit();
